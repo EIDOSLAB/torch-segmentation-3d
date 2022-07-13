6334f94c7f23a5e8247201c9b85521b8a2c304c1
Carlo Alberto Barbano <carlo.alberto.barbano@outlook.com>
2022-07-13 23:10:05 +0200

    remove LazyLinear (tmp fix)

--- pretrain/encoder_ce.py
+++ pretrain/encoder_ce.py
@@ -6,7 +6,11 @@ class EncoderCE(nn.Module):
     def __init__(self, encoder_name, n_classes):
         super().__init__()
         self.encoder = seg3d.encoders.get_encoder(encoder_name, in_channels=1, weights=None)
-        self.classifier = nn.Sequential(nn.AdaptiveAvgPool3d(1), nn.Flatten(), nn.LazyLinear(n_classes))
+
+        feats = {"resnet18": 512}
+        self.classifier = nn.Sequential(
+            nn.AdaptiveAvgPool3d(1), nn.Flatten(), nn.Linear(feats[encoder_name], n_classes)
+        )
 
     def forward(self, x):
         features = self.encoder(x)
