70516b3fef2696e1cacf807fce2fd54110c0f30c
Andrea Bragagnolo <andr.bragagnolo@gmail.com>
2022-07-14 12:39:45 +0200

    update build Dockerfile path

--- pretrain/build.sh
+++ pretrain/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-docker build -t eidos-service.di.unito.it/barbano/torch-segmentation-3d:pretrain . -f Dockerfile
+docker build -t eidos-service.di.unito.it/barbano/torch-segmentation-3d:pretrain . -f pretrain/Dockerfile
 docker push eidos-service.di.unito.it/barbano/torch-segmentation-3d:pretrain
