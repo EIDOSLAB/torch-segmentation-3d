7b8200d110cc29c28d51a8adf9cb85be908a9443
Carlo Alberto Barbano <carlo.alberto.barbano@outlook.com>
2022-07-14 15:44:52 +0200

    fix typo

--- pretrain/build.sh
+++ pretrain/build.sh
@@ -3,5 +3,5 @@
 docker build -t eidos-service.di.unito.it/barbano/torch-segmentation-3d:pretrain . -f pretrain/Dockerfile.pretrain
 docker push eidos-service.di.unito.it/barbano/torch-segmentation-3d:pretrain
 
-docker build -t eidos-service.di.unito.it/barbano/torch-segmentation-3d:sweep . -f pretrain/Dockerfile.sweeo
+docker build -t eidos-service.di.unito.it/barbano/torch-segmentation-3d:sweep . -f pretrain/Dockerfile.sweep
 docker push eidos-service.di.unito.it/barbano/torch-segmentation-3d:sweep
