5c4a1045d9eeedb9e1befef08ce3776c9f81b25b
Carlo Alberto Barbano <carlo.alberto.barbano@outlook.com>
2022-07-14 15:44:18 +0200

    add sweep agent image

--- pretrain/build.sh
+++ pretrain/build.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
-docker build -t eidos-service.di.unito.it/barbano/torch-segmentation-3d:pretrain . -f pretrain/Dockerfile
+docker build -t eidos-service.di.unito.it/barbano/torch-segmentation-3d:pretrain . -f pretrain/Dockerfile.pretrain
 docker push eidos-service.di.unito.it/barbano/torch-segmentation-3d:pretrain
+
+docker build -t eidos-service.di.unito.it/barbano/torch-segmentation-3d:sweep . -f pretrain/Dockerfile.sweeo
+docker push eidos-service.di.unito.it/barbano/torch-segmentation-3d:sweep
