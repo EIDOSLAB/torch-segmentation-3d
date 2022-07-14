128d56c40cf3a51d1ad67c15f9d4e2dcea04a5eb
Andrea Bragagnolo <andr.bragagnolo@gmail.com>
2022-07-14 11:19:16 +0200

    removed useless name from pretrain's Dockerfile

--- pretrain/build.sh
+++ pretrain/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-docker build -t eidos-service.di.unito.it/barbano/torch-segmentation-3d:pretrain . -f Dockerfile.pretrain
+docker build -t eidos-service.di.unito.it/barbano/torch-segmentation-3d:pretrain . -f Dockerfile
 docker push eidos-service.di.unito.it/barbano/torch-segmentation-3d:pretrain
