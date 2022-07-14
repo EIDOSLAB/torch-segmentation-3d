38a082c50bfa2bcfabbefd22c9820fa075905dac
Carlo Alberto Barbano <carlo.alberto.barbano@outlook.com>
2022-07-14 11:09:45 +0200

    remove pretrain build files

--- build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-docker build -t eidos-service.di.unito.it/barbano/torch-segmentation-3d:pretrain . -f Dockerfile.pretrain
-docker push eidos-service.di.unito.it/barbano/torch-segmentation-3d:pretrain
